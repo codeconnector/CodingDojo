630fc88dc14a911dcad5d5a530793dfdade32af0
Laura Pilkington <ladylaurel18@gmail.com>
2021-04-21 10:16:09 -0400

    Laurelin potential palindrome (#44)
    * solution with some tests
    
    * changed func name
    
    * removing.pyc files, I hope

---
+++ challenges/2021-04-20-potential-palindrome/solutions/laurelin.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+
+def can_make_palindrome(s: str) -> bool:
+    #count char frequency in string, if more than one is odd, not a palindrome
+    if not s:
+        return False
+    freq = defaultdict(int)
+    for char in s:
+        freq[char] += 1
+    if len([val for val, chr in freq.items() if chr % 2 != 0]) > 1:
+        return False
+    return True
