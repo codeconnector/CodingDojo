2b6abcfeb205dc450b0c39e218f295dae0ae611e
Eric Burden <eric.w.burden@gmail.com>
2021-06-18 09:09:46 -0500

    Submit challenge for 2021-06-22 meetup

---
+++ challenges/2021-06-22-coin-counter/src/main/kotlin/Solution.kt
@@ -0,0 +1,4 @@
+fun countCoinRounds(coins: Int): Int {
+    // Insert code here to solve the challenge
+    return 0
+}
--- resources/kotlin/run_tests.sh
+++ resources/kotlin/run_tests.sh
@@ -2,3 +2,4 @@
 
 ../../resources/kotlin/gradlew test --info
 ln -s build/reports/tests/test/index.html test_report.html
+echo "You can also view test results in the 'test_report.html' file."
