a53a517b8ae1c068f02d384dffa26fb19da76af5
Eric Burden <eric.w.burden@gmail.com>
2021-07-20 08:25:15 -0500

    Closes #105, challenge setup for Balanced Brackets (#113)
    This needs merging

---
+++ challenges/2021-07-20-balanced-brackets/solution.js
@@ -0,0 +1,30 @@
+// # Balanced Brackets
+// 
+// Given a string of round, curly, and square open and closing brackets, return
+// whether the brackets are balanced (well-formed).
+// 
+// For example, given the string "([])[]({})", you should return true.
+// Given the string "([)]" or "((()", you should return false.
+// 
+// ## Business Rules/Errata
+// 
+// - The only characters considered to be 'brackets' are `(`, `)`, `[`, `]`, `{`,
+//   and `}`.
+// - Your input will always be a string.
+// - An empty string is considered balanced (return true).
+// - **Your string may contain characters that are not brackets.**
+// 
+// ## Examples
+// 
+// ```
+// balanced_brackets("[[]]({}[])");       // true
+// balanced_brackets("[[({}[])");         // false
+// balanced_brackets("");                 // true
+// balanced_brackets("(5 * 3) + 4");      // true
+// ```
+
+function balancedBrackets(input) {
+    // Insert your solution code here
+}
+
+module.exports = balancedBrackets;
