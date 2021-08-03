3b2e303b4c0fdbb55251ac7211ecab31bfdeb923
Azhya Knox <azhya.knox@revature.com>
2021-08-03 13:27:15 -0500

    mob solution attempt with working output printing

--- challenges/2021-08-03-tracing-triangles/TracingTriangles.cs
+++ challenges/2021-08-03-tracing-triangles/TracingTriangles.cs
@@ -1,11 +1,49 @@
 using Xunit;
+using Xunit.Abstractions;
 
 public class TracingTriangles
 {
+    private readonly ITestOutputHelper output;
+
+    public TracingTriangles(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
     public int LongestPath(int[][] triangle)
     {
         // Insert your solution code here
-        return 0;
+        //1. first we have a way to establish what is a child
+        int []max = new int [triangle.Length];
+        output.WriteLine($"Length of Triangle is {triangle.Length}.");
+        output.WriteLine($"Current Length of max: {max.Length}.");
+
+        //2. first element has to have length of 1
+        for (var i = 0; i < triangle.Length; i++)
+        {
+            output.WriteLine($"Current position of triangle[i]: {triangle[i].ToString()}.");
+            //3. index + 1 determines the length of the next row at the index
+            for (int j = 0; j < triangle[i+1].Length - 1; j++)
+            {
+                output.WriteLine($"Current position of triangle[i][j]: {triangle[i][j]}.");
+                if(triangle[i][j] < triangle[i][j+1]){
+                    max[j] += triangle[i][j+1];
+                }else
+                {
+                    max[j] += triangle[i][j];
+                }
+            }
+        }
+        //4. returns the weight of the maximum weight path
+        int maxWeight = 0;
+        for (int i = 0; i < max.Length; i++)
+        {
+            if(max[i] > maxWeight){
+                maxWeight = max[i];
+            }
+        }
+        output.WriteLine($"Calculated maxWeight: {maxWeight}.");
+        return maxWeight;
     }
 
     [Fact]
