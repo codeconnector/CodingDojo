c657021a96dacd35af0c02b4e534fb1733aaf5e1
Dennis Kennetz <dennis.kennetz@stjude.org>
2021-09-27 15:32:40 -0500

    challenge setup for minimumImpossibleSum

---
+++ challenges/2021-09-28-minimum-impossible-sum/solution.js
@@ -0,0 +1,27 @@
+//# Minimum Impossible Sum
+//
+//Given a sorted array of integers, find the smallest positive integer that is not the sum of a subset of the array.
+//
+//## Business Rules/Errata
+//
+//- Your input will be an array of integers, sorted in ascending order (smallest to largest).
+//- Your input will always contain at least one number.
+//- Your answer should be a positive integer larger than zero.
+//- You do not need to account for integer overflow (i.e., the answer should always be a 32-bit positive number,  in the range from 0 to 4,294,967,295).
+//- Bonus: Can you solve this challenge in linear (O(n)) time?
+//
+//## Examples
+//
+//```
+//min_impossible_sum([5]) // 1
+//min_impossible_sum([1]) // 2
+//min_impossible_sum([1, 2, 3, 10]) // 7
+//min_impossible_sum([1, 2, 3, 7, 10]) // 24
+//
+//```
+
+function minImpossibleSum(array) {
+    // Insert your solution code here
+}
+
+module.exports = { minImpossibleSum };
