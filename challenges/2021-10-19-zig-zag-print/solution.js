dbbf7f22f48249a103eec59f65e5ec4b029d17fe
Eric Burden <eric.w.burden@gmail.com>
2021-10-19 10:05:23 -0500

    Submit challenge for 2021-10-19 meetup, Zig Zag Print (#220)
    Need this now.

---
+++ challenges/2021-10-19-zig-zag-print/solution.js
@@ -0,0 +1,56 @@
+// # ZigZag Print
+// 
+// Given a string and a number of lines k, print the string in zigzag form. In zigzag,
+// characters are printed out diagonally from top left to bottom right until reaching
+// the kth line, then back up to top right, and so on.
+// 
+// For example, given the sentence "thisisazigzag" and k = 4, you should print:
+// 
+// ```
+// t     a     g
+//  h   s z   a
+//   i i   i z
+//    s     g
+// ```
+// 
+// ## Business Rules/Errata
+// 
+// - The input will include a string and a positive integer between 2 and 10. 
+// - Your output should be in the form of a string that, when printed to console, will
+//   yield the desired pattern. See examples below.
+// - Your string may contain any valid ASCII letter, number, space, or printable
+//   special character ('!', '@', '#', '$', etc.). Importantly, control characters such
+//   as '\t', '\n', '\r', '\v', etc. will not be included.
+// - Spaces should be included in your output pattern.
+// 
+// ## Examples
+// 
+// ```
+// printzigzag("bismarcks", 3)  // "b   a   s\n i m r k \n  s   c  "
+// b   a   s
+//  i m r k
+//   s   c
+// 
+// 
+// printzigzag("thisisazigzag", 4)  
+// // "t     a     g\n h   s z   a \n  i i   i z  \n   s     g   "
+// t     a     g
+//  h   s z   a
+//   i i   i z
+//    s     g
+// 
+// 
+// printzigzag("ilovechickenandwaffles", 5) 
+// // "i       c       a     \n l     i k     w f    \n  o   h   e   d   f   \n   v c     n n     l s\n    e       a       e "
+// i       c       a
+//  l     i k     w f
+//   o   h   e   d   f
+//    v c     n n     l s
+//     e       a       e
+// ```
+
+function printZigZag(aString) {
+    // Insert your solution code here
+}
+
+module.exports = { printZigZag };
