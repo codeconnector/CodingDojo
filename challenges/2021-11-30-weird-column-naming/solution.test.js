7c6e4776f2992ce4a20e4d4545c0ff69232bd3b2
Charles Umiker <69427808+mrumiker@users.noreply.github.com>
2021-11-30 10:10:08 -0600

    add Fun tests
    I thought I'd add some fun tests to the challenge. Feel free to ignore if this muddies the TDD lesson too much.

--- challenges/2021-11-30-weird-column-naming/solution.test.js
+++ challenges/2021-11-30-weird-column-naming/solution.test.js
@@ -15,3 +15,19 @@ test('Return AZ if number is 52', () => {
 test('Return KA if number is 287', () => {
     expect(to_spreadsheet_colname(287)).toStrictEqual("KA");
 })
+
+test('Return YYZ if number is 17576', () => {
+    expect(to_spreadsheet_colname(17576)).toStrictEqual("YYZ");
+})
+
+test('Return DOJO if number is 80719', () => {
+    expect(to_spreadsheet_colname(80719)).toStrictEqual("DOJO");
+})
+
+test('Return BECKY if number is 1004171', () => {
+    expect(to_spreadsheet_colname(1004171)).toStrictEqual("BECKY");
+})
+
+test('Return TOOPS if number is 9413735', () => {
+    expect(to_spreadsheet_colname(9413735)).toStrictEqual("TOOPS");
+})
