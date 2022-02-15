21fb30647c35ba235a35fcdd742948d83d4ede9e
Charles Umiker <69427808+mrumiker@users.noreply.github.com>
2022-02-14 20:32:23 -0600

    Setup for Three Number Sort Challenge (#263)
    * Three numbers sort setup - needs help
    
    * fixes For challenge setup
    
    * change Threes parameter to tuple type
    
    Co-authored-by: Dennis Kennetz <dennis.kennetz@stjude.org>

---
+++ challenges/2022-02-14-three-number-sort/ThreeNumberSort.cs
@@ -0,0 +1,102 @@
+using Xunit;
+
+public class ThreeNumberSort
+{
+  public int[] SortArray(int[] input, (int, int, int) threes)
+  {
+    // Insert your solution code here
+    return input;
+  }
+
+  [Fact]
+  public void SortsArrayWithAllThreeNumbers()
+  {
+    var input = new int[] { 3, 2, 3, 1, 2, 2, 1, 3, 2 };
+    var threes = (1, 2, 3);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 1, 1, 2, 2, 2, 2, 3, 3, 3 };
+    Assert.Equal(actual.Length, expected.Length);
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+
+  [Fact]
+  public void SortsArrayWithFirstNumberMissing()
+  {
+    var input = new int[] { 2, 3, 3, 2, 2, 3 };
+    var threes = (1, 2, 3);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 2, 2, 2, 3, 3, 3 };
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+
+  [Fact]
+  public void SortsArrayWithLastNumberMissing()
+  {
+    var input = new int[] { 1, 2, 1, 1, 2, 1 };
+    var threes = (1, 2, 3);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 1, 1, 1, 1, 2, 2 };
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+
+  [Fact]
+  public void SortsArrayWithMiddleNumberMissing()
+  {
+    var input = new int[] { 3, 1, 1, 3 };
+    var threes = (1, 2, 3);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 1, 1, 3, 3 };
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+
+  [Fact]
+  public void SortsArrayWithNonSortedTriad()
+  {
+    var input = new int[] { 3, 1, 1, 3, 2 };
+    var threes = (3, 2, 1);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 3, 3, 2, 1, 1 };
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+
+  [Fact]
+  public void SortsArrayWithNonSortedTriadAndMissingNumber()
+  {
+    var input = new int[] { 1, 2, 1, 1, 2, 1 };
+    var threes = (2, 3, 1);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 2, 2, 1, 1, 1, 1 };
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+
+  [Fact]
+  public void SortsArrayWithOnlyOneNumberPresent()
+  {
+    var input = new int[] { 1, 1, 1, 1 };
+    var threes = (2, 3, 1);
+    var actual = SortArray(input, threes);
+    var expected = new int[] { 1, 1, 1, 1 };
+    for (var i = 0; i < actual.Length; i++)
+    {
+      Assert.Equal(actual[i], expected[i]);
+    }
+  }
+}
