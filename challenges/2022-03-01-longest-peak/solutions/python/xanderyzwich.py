2f34c9cfddea5abf7fe8fd8cf276da8ef9ce276b
Corey McCarty <corey@mccarty.dev>
2022-03-01 15:32:59 -0600

    refactored

--- challenges/2022-03-01-longest-peak/solutions/python/xanderyzwich.py
+++ challenges/2022-03-01-longest-peak/solutions/python/xanderyzwich.py
@@ -20,35 +20,25 @@ from unittest import TestCase
 
 
 def longest_peak(elevations):
-    incline, decline, longest, i = 0, 0, 0, 0
+    uphill, downhill, longest, i = 0, 0, 0, 0
 
-    while i < len(elevations) - 1:  # stop one before end to prevent next being out of bounds
-        is_end = False
+    while i < len(elevations) - 1:  # stop one before end to prevent out of bounds next
         current, next = elevations[i], elevations[i+1]
+        rising, falling, plateau = current < next, current > next, current == next
+        peak = uphill and downhill
 
-        if current < next:  # uphill
-            if decline > 0:  # new rise ends previous peak
-                is_end = True
-            else:
-                incline += 1
-        elif current > next:  # downhill
-            if incline > 0:  # only count if attached to uphill
-                decline += 1
-        else:  # plateau
-            if incline and not decline:  # not a peak
-                incline = 0
-            if incline and decline:  # ends previous peak
-                is_end = True
+        if rising and not downhill:
+            uphill += 1
+        elif falling and uphill:
+            downhill += 1
+            peak = True
+        elif plateau and not peak:
+            uphill = 0
 
-        # Check for peak ending at end of list
-        if len(elevations) - 2 == i and incline and decline:
-            is_end = True
-
-        if is_end:
-            peak_length = incline + decline + 1
-            if peak_length > longest:
-                longest = peak_length
-            incline, decline = 0, 0
+        list_end = len(elevations) - 2 == i
+        if peak and any([list_end, rising, plateau]):
+            longest = max(longest, uphill + downhill + 1)
+            uphill, downhill = 0, 0
         else:
             i += 1
 
@@ -58,16 +48,18 @@ def longest_peak(elevations):
 class TestLongestPeak(TestCase):
 
     def test_not_peak(self):
-        assert longest_peak([5, 4, 3, 2, 1]) == 0  # all decreasing
-        assert longest_peak([1, 2, 3, 4, 5]) == 0  # all increasing
-        assert longest_peak([5, 4, 3, 2, 1, 2, 10, 12]) == 0  # all decreasing, then all increasing
-        assert longest_peak([]) == 0  # empty input check mandatory :D
-        assert longest_peak([1, 2, 3, 3, 2, 1]) == 0  # equality at peak tip, 0 is returned
+        assert longest_peak([5, 4, 3, 2, 1]) == 0               # all decreasing
+        assert longest_peak([1, 2, 3, 4, 5]) == 0               # all increasing
+        assert longest_peak([5, 4, 3, 2, 1, 2, 10, 12]) == 0    # all decreasing, then all increasing
+        assert longest_peak([]) == 0                            # empty input check mandatory :D
+        assert longest_peak([1]) == 0                           # single element
+        assert longest_peak([1, 2]) == 0                        # two element
+        assert longest_peak([1, 2, 3, 3, 2, 1]) == 0            # equality at peak tip, 0 is returned
 
     def test_peaks(self):
-        assert longest_peak([1, 3, 2]) == 3  # normal peak
-        assert longest_peak([1, 2, 3, 4, 5, 1]) == 6  # peak ends on last number
-        assert longest_peak([2, 2, 3, 2]) == 3  # repeat at the beginning
-        assert longest_peak([1, 2, 3, 2, 1, 1]) == 5  # repeat at the end
-        assert longest_peak([1, 1, 1, 2, 3, 10, 12, -3, 2, 3, 45, 800, 99, 98, 0, -1, 2, 3, 4, 5, 0, -1]) == 9
+        assert longest_peak([1, 3, 2]) == 3                     # normal peak
+        assert longest_peak([1, 2, 3, 4, 5, 1]) == 6            # peak ends on last number
+        assert longest_peak([2, 2, 3, 2]) == 3                  # repeat at the beginning
+        assert longest_peak([1, 2, 3, 2, 1, 1]) == 5            # repeat at the end
         # peak in the middle -3, 2, 3, 45, 800, 99, 98, 0, -1
+        assert longest_peak([1, 1, 1, 2, 3, 10, 12, -3, 2, 3, 45, 800, 99, 98, 0, -1, 2, 3, 4, 5, 0, -1]) == 9
