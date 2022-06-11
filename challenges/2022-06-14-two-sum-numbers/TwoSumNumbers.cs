b3f6177cc9105eb90688bdf402441c598eea65d2
mrumiker <69427808+mrumiker@users.noreply.github.com>
2022-06-11 14:43:39 -0500

    challenge Setup for Two Sum Numbers

---
+++ challenges/2022-06-14-two-sum-numbers/TwoSumNumbers.cs
@@ -0,0 +1,100 @@
+using Xunit;
+
+public class TwoSumNumbers
+{
+  public bool DoesItSum(int sum, int[] numbers)
+  {
+    // Insert your solution code here
+    return false;
+  }
+
+  [Fact]
+  public void FindsNormalTrue()
+  {
+    var sum = 17;
+    var numbers = new int[] { 10, 15, 3, 7 };
+    var expected = true;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsNormalFalse()
+  {
+    var sum = 17;
+    var numbers = new int[] { 10, 15, 3, 8 };
+    var expected = false;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsAnotherNormalFalse()
+  {
+    var sum = 16;
+    var numbers = new int[] { 10, 15, 8, 7 };
+    var expected = false;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsTrueWhenBothNumbersEqual()
+  {
+    var sum = 16;
+    var numbers = new int[] { 10, 15, 8, 8 };
+    var expected = true;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsFalseWhenArrayEmpty()
+  {
+    var sum = 32;
+    var numbers = new int[] { };
+    var expected = false;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsFalseWhenArrayHasLengthOne()
+  {
+    var sum = 32;
+    var numbers = new int[] { 32 };
+    var expected = false;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsTrueWhenOneNumIsZero()
+  {
+    var sum = 32;
+    var numbers = new int[] { 32, 0 };
+    var expected = true;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsFalseWhenSumHasWrongSign()
+  {
+    var sum = -32;
+    var numbers = new int[] { 32, 0 };
+    var expected = false;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void FindsTrueWhenSumIsNegative()
+  {
+    var sum = -32;
+    var numbers = new int[] { 32, -64 };
+    var expected = true;
+    var actual = DoesItSum(sum, numbers);
+    Assert.Equal(expected, actual);
+  }
+}
