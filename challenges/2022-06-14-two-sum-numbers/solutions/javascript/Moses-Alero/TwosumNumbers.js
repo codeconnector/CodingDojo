b8911b2c423efe9f77089ad7defe0c2dc9518534
Moses-Alero <mosesalero@gmail.com>
2022-06-16 09:25:01 +0100

    Add Javascript solution to Two sum Numbers

--- challenges/2022-06-14-two-sum-numbers/solutions/javascript/Moses-Alero/TwosumNumbers.js
+++ challenges/2022-06-14-two-sum-numbers/solutions/javascript/Moses-Alero/TwosumNumbers.js
@@ -1,11 +1,12 @@
 const TwoSumNumbers = function (number, numbersArray){
 
-    for(let i = 0; i< numbersArray.length; i++){
-        const summation = number - numbersArray[i];
-        numbersArray.shift(numbersArray[i]);
-        if(numbersArray.includes(summation)) {
+   for(let i = 0; i < numbersArray.length;i++){
+        const summation = number - numbersArray[0];
+        numbersArray.splice(0,1);
+        console.log(numbersArray);
+        if(numbersArray.includes(summation)){
             return true;
-        }
+        } 
     }
     return false;
 }
