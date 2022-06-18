d9e2eef0e0d3880e13c88cb501115860e03d6991
Moses-Alero <mosesalero@gmail.com>
2022-06-18 02:27:45 +0100

    Add javascript solution

--- challenges/2022-06-14-two-sum-numbers/solutions/javascript/Moses-Alero/TwosumNumbers.js
+++ challenges/2022-06-14-two-sum-numbers/solutions/javascript/Moses-Alero/TwosumNumbers.js
@@ -1,11 +1,76 @@
-const TwoSumNumbers = function (number, numbersArray){
+const TwoSumNumbers = function (addend, numbersArray){
 
-   while(numbersArray.length > 0){
-        const summation = number - numbersArray[0];
-        numbersArray.splice(0,1);
-        if(numbersArray.includes(summation)){
+    while(numbersArray.length > 0){
+        const number = addend - numbersArray[0];
+        numbersArray.shift();
+        if(numbersArray.includes(number)){
             return true;
-        } 
+        }
     }
     return false;
-}
+    }
+
+const testArr = [
+    {
+    number: 2,
+    numbersArray: [1, 1],
+    expected: true,
+    },
+    {
+    number: 2,
+    numbersArray: [1, 100, 1000, 100000, 1, 55, 8000000, 6],
+    expected: true,
+    },
+    {
+    number: 17,
+    numbersArray: [10, 15, 3, 8],
+    expected: false,
+    },
+    {
+    number: 17,
+    numbersArray: [10, 15, 3, 7],
+    expected: true,
+    },
+    {
+    number: 16,
+    numbersArray: [10, 15, 8, 7],
+    expected: false,
+    },
+    {
+    number: 16,
+    numbersArray: [10, 15, 8, 8],
+    expected: true,
+    },
+    {
+    number: 32,
+    numbersArray: [],
+    expected: false,
+    },
+    {
+    number: 32,
+    numbersArray: [32],
+    expected: false,
+    },
+    {
+    number: 32,
+    numbersArray: [32, 0],
+    expected: true,
+    },
+    {
+    number: -32,
+    numbersArray: [32, 0],
+    expected: false,
+    },
+    {
+    number: -32,
+    numbersArray: [32, -64],
+    expected: true,
+    },
+    ]
+    testArr.forEach((test, i) => {
+    const actual = TwoSumNumbers(test.number, test.numbersArray);
+    const expected = test.expected;
+    console.assert(actual === expected, `Test ${i} failed. Expected ${expected}, got ${actual}.`)
+    });
+    console.log("Tests complete")
+    
