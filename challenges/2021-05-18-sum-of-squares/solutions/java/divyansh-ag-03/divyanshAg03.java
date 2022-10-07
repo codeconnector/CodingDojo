d068a8f275028097d08b9994255ba66a3041617a
Divyansh Agrawal <divyansh.agrawal411@gmail.com>
2022-10-07 11:07:09 +0530

    Changed the code as prev was filling stack up with recursive calls

--- challenges/2021-05-18-sum-of-squares/solutions/java/divyansh-ag-03/divyanshAg03.java
+++ challenges/2021-05-18-sum-of-squares/solutions/java/divyansh-ag-03/divyanshAg03.java
@@ -1,30 +1,54 @@
-public class divyanshAg03{
-    static int SumOfSquares(int n)
+class divyanshAg03
+{
+
+    static int getMinSquares(int n)
     {
-        if (n <= 3)    //base cases (ex:if n = 2 i.e. 1^2+1^2 = 2)
-        return n;
+        if (n <= 3) //ex: if n = 2, O/P = 1^2 + 1^2 = 2
+            return n;
+ 
+        // Create a dynamic programming
+        // to store squares
+        int dp[] = new int[n + 1];
+ 
+        // getMinSquares table for
+        // base case entries
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
 
-        int res = n;
-
-        for (int i = 1; i<= n; i++)
+        // rest of table using recursion
+        for (int i = 4; i <= n; i++)
         {
-            int temp = i * i;
-            if (temp > n)
+         
+            // max value is i as i can
+            // always be represented
+            dp[i] = i;
+ 
+            // Going through all smaller numbers to recursively find minimum
+            for (int x = 1; x <= Math.ceil(
+                              Math.sqrt(i)); x++)
             {
-                break;
-            }
-            else
-            {
-                res = Math.min(res, 1 + SumOfSquares(n - temp));
+                int temp = x * x;
+                if (temp > i)
+                    break;
+                else
+                    dp[i] = Math.min(dp[i], 1
+                                  + dp[i - temp]);
             }
         }
+        int res = dp[n]; //storing result
+ 
         return res;
     }
-
-    public static void main(String[] args) 
-    {   //running tests
-        System.out.println(SumOfSquares(8));
-        System.out.println(SumOfSquares(12));
-        System.out.println(SumOfSquares(9));
+   
+    //running tests ...
+    public static void main(String args[])
+    {
+        System.out.println(getMinSquares(13));
+        System.out.println(getMinSquares(27));
+        System.out.println(getMinSquares(144));
+        System.out.println(getMinSquares(84));
+        System.out.println(getMinSquares(85));
     }
 }
