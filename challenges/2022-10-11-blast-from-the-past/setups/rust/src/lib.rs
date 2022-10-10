3a236c36ae3865c1f7ce3c5591638c3f26bb81a1
Eric Burden <eric@ericburden.dev>
2022-10-10 19:37:13 -0400

    Fixed setup to allow 'null' for this challenge and test for it

--- challenges/2022-10-11-blast-from-the-past/setups/rust/src/lib.rs
+++ challenges/2022-10-11-blast-from-the-past/setups/rust/src/lib.rs
@@ -16,7 +16,7 @@
 // - Linear time -> you should solve this problem by only passing through the array a
 //   single time.
 
-pub fn find_three_largest_numbers(arr: &[i32]) -> [Option<i32>; 3] {
+pub fn find_three_largest_numbers(arr: &[i32]) -> Option<[i32; 3]> {
     unimplemented!()
 }
 
@@ -28,23 +28,22 @@ mod tests {
     fn test_case_one() {
         let input = [141, 2, 17, -7, -17, -27, 18, 541, 8, 7, 7];
         let result = find_three_largest_numbers(&input);
-        let expected = [Some(18), Some(141), Some(541)];
-        assert_eq!(result, expected);
+        let expected = [18, 141, 541];
+        assert_eq!(result.expect("Should contain a value!"), expected);
     }
 
     #[test]
     fn test_case_two() {
         let input = [11, -7, 5, -7];
         let result = find_three_largest_numbers(&input);
-        let expected = [Some(-7), Some(5), Some(11)];
-        assert_eq!(result, expected);
+        let expected = [-7, 5, 11];
+        assert_eq!(result.expect("Should contain a value!"), expected);
     }
 
     #[test]
     fn test_case_three() {
         let input = [1];
         let result = find_three_largest_numbers(&input);
-        let expected = [Some(1), None, None];
-        assert_eq!(result, expected);
+        assert_eq!(result, None);
     }
 }
