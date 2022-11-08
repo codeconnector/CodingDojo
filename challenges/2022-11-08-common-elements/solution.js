88057a43bbd8949949ddba5464a58bf32c28793c
Charles Umiker <69427808+mrumiker@users.noreply.github.com>
2022-11-08 10:35:21 -0600

    Update solution.js

--- challenges/2022-11-08-common-elements/solution.js
+++ challenges/2022-11-08-common-elements/solution.js
@@ -1,11 +1,12 @@
 // # Common Elements
 
-// Create a function that takes two "sorted" arrays of numbers and returns an array of numbers which are common to boththe input arrays.
+// Create a function that takes two "sorted" arrays of numbers and returns an array of numbers which are common to both the input arrays.
 
 // ## Business Rules/Errata
 
 // - You can expect the arrays to be sorted
 // - Arrays do not have to be the same length
+// - The output array should be sorted and contain no duplicates
 // - This problem should be solved in O(n+m) time where n is the length of the first input array and m is the length of the second input array
 
 // ## Examples
