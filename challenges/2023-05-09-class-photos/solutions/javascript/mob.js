70c77c2087cd099497e7ea02c1182e2e1ec60bcc
Richard <richardlwhittington@gmail.com>
2023-05-09 12:57:03 -0500

    dojo mobsolution for class photos

---
+++ challenges/2023-05-09-class-photos/solutions/javascript/mob.js
@@ -0,0 +1,40 @@
+// Class Photos
+// It's photo day at the local school, and you are the photographer assigned to take class photos. 
+// The class that you'll be photographing has an even number of students, and all these students are wearing red or blue shirts. 
+// In fact, exactly half of the class is wearing red shirts, and the other half is wearing blue shirts (wow!). 
+// You're responsible for arranging the students in two rows before taking the photo. 
+// Each row should contain the same number of the students and should adhere to the following guidelines:
+//  All students wearing red shirts must be in the same row
+//  All students wearing blue shirts must be in the same row
+//  Each student in the back row must be strictly taller than the student directly in front of them in the front row
+//  You're given two input arrays: one containing the heights of all students with red shirts and another one containing the heights of all the students with blue shirts. The arrays will always have the same length, and each height will be a positive integer. Write a function that returns whether or not a class photo that follows the stated guidelines can be taken.
+//  Note: you can assume that each class has at least 2 students.
+
+// EXAMPLE:
+// red_shirt_heights = [5, 8, 1, 3, 4]
+// blue_shirt_heights = [6, 9, 2, 4, 5]
+
+// # output
+// True
+
+function classPhotos(redHeights, blueHeights) {
+  redHeights.sort((a,b)=>b - a);
+  blueHeights.sort((a,b)=>b - a);
+  let rowInFront;
+  if(redHeights[0] > blueHeights[0]){
+     rowInFront = "blue";
+  }else{
+    rowInFront = "red";
+  }
+  for(let i = 0;i < redHeights.length;i++){
+     if(redHeights[i] >= blueHeights[i] && rowInFront == "red"){
+       return false;
+     }
+     if(blueHeights[i] >= redHeights[i] && rowInFront == "blue"){
+      return false;
+     }
+  }
+  return true;
+}
+
+module.exports = { classPhotos };
