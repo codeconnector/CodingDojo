69d82855bf868a792e1e3a3c48a1ab3b011cf9c2
Corey McCarty <corey@mccarty.dev>
2024-01-09 11:58:53 -0600

    maximum non-adjacent sum setup

---
+++ challenges/2024-01-09-maximul-non-adjacent-sum/solution.test.js
@@ -0,0 +1,26 @@
+const { default: test } = require('node:test');
+const { MaxNonAdjacentSum } = require('./solution');
+
+test('Empty Array returns Zero', () =>{
+    const arr = [];
+    const expected = 0;
+    const actual = MaxNonAdjacentSum(arr);
+    expect(actual).toEqual(expected);
+});
+
+test('Non-Adjacent Sum is Max', () =>{
+    const arr = [75, 105, 120, 75, 90, 135];
+    const expected = 330;
+    const actual = MaxNonAdjacentSum(arr);
+    expect(actual).toEqual(expected);
+});
+
+
+test('Adjacent Sum is Max', () =>{
+    const arr = [3, 5, 1, 1];
+    const expected = 6;
+    const actual = MaxNonAdjacentSum(arr);
+    expect(actual).toEqual(expected);
+})
+
+
